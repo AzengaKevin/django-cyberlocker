ec8ea0265551d94971627801af20807bb033f2b5
Azenga Kevin <azenga.kevin7@gmail.com>
2024-07-03 23:53:15 +0300

    wip: adding files to the admin section

--- files/admin.py
+++ files/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from files.models import File
+
+
+@admin.register(File)
+class FileAdmin(ModelAdmin):
+    list_display = ('original_filename', 'file_extension', 'mime_type', 'description')
+    search_fields = ('original_filename', 'description')
+
